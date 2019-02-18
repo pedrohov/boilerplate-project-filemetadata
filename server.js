7bc69b3435cbaa89b5792cc41c5a9afbe67a3936
Pedro Veloso <pedrohov@users.noreply.github.com>
2019-02-18 16:28:38 -0300

    Add fileanalyse endpoint

--- server.js
+++ server.js
@@ -2,8 +2,8 @@
 
 var express = require('express');
 var cors = require('cors');
-
-// require and use "multer"...
+var multer = require('multer');
+var upload = multer();
 
 var app = express();
 
@@ -11,11 +11,18 @@ app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
-     res.sendFile(process.cwd() + '/views/index.html');
-  });
+  res.sendFile(process.cwd() + '/views/index.html');
+});
 
-app.get('/hello', function(req, res){
-  res.json({greetings: "Hello, API"});
+app.post('/api/fileanalyse', upload.single('upfile'), function(req, res) {
+  if(req.file) {
+    return res.json({
+      name: req.file.originalname,
+      type: req.file.mimetype,
+      size: req.file.size
+    });
+  }
+  return res.send("Choose a file to upload.");
 });
 
 app.listen(process.env.PORT || 3000, function () {
